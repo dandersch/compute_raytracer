3597aedd5fa154ae6dfdafdf9f53a08ac051ba9e
da <>
2024-06-23 17:15:39 +0200

    integrate build.sh into build.bat

--- build.sh
+++ build.sh
@@ -1,8 +1,1 @@
-#!/bin/bash
-
-# compile as (hot-reloadable) dll + exe
-#cc --shared -fPIC -DCOMPILE_DLL -Wall -Wshadow main.c -o code.dll -lGLEW -lGL
-#cc -DCOMPILE_EXE -Wall -Wshadow main.c -o main -lglfw -lGL -ldl -lm
-
-# compile as standalone executable
-cc -DCOMPILE_EXE -DCOMPILE_DLL -Wall -Wshadow main.c -o main -lglfw -lGLEW -lGL -lm
+build.bat
